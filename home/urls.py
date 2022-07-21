48dd8daaac80b25aa8d9ac52212c9abae6f73107
Agustin Caballero <aguscaballero117@gmail.com>
2022-07-20 21:42:57 -0300

    Agregue herencias y templates

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, about, perfil, login
 urlpatterns = [
     path('', homepage, name='Inicio'),
+    path('about/', about, name='about'),
+    path('perfil/', perfil, name='perfil'),
+    path('login/', login, name='login')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'index.html')
+
+def about(request):
+    return  render(request, 'about.html')
+
+def perfil(request):
+    return  render(request, 'perfil.html')
+
+def login(request):
+    return  render(request, 'login.html')
