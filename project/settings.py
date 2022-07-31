a8d5ec8aa280276ee7f14be02c51e2de9b8f0728
Agustin Caballero <aguscaballero117@gmail.com>
2022-07-31 06:29:10 -0300

    Agregue foto de perfil

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\crate\Desktop\Entrega1 - Agustin_Santa Maria, Caballero, Garcia\templates'],
+        'DIRS': [r'C:\Users\usuario\Desktop\Proyecto\Entrega1---Santa-Maria-Garcia-Caballero\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
